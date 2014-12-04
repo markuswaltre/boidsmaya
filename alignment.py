a5e8a14b7882c1ca729ae343bcc4904b3c4cbdc0
TobyPalmer <tobpa249@student.liu.se>
2014-12-04 16:29:32 +0100

    alignment not ready

---
+++ alignment.py
@@ -0,0 +1,19 @@
+from vec import *
+from boid import *
+
+def calculateAlignment(currentBoidIndex, boids, NEIGHBOR_DISTANCE):
+	
+	neighbors = []
+	alignmentVector = [0,0,0]
+
+	for index in range(len(boids)):
+	if(index != currentBoidIndex):
+		if(dist(currentBoidPos , boids[index].getPosition()) < NEIGHBOR_DISTANCE):
+			neighbors.append(boids[index])
+
+	numberOfNeighbors = len(neighbors)
+
+	for neighbor in neighbors:
+		temp = add(temp,neighbor.getVelocity())
+		temp = scale_by_scalar(temp, 1/numberOfNeighbors)
+
