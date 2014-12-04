8f44e9c9772133b70435edd066b55282b514f17b
Markus Waltre <markus.waltre@gmail.com>
2014-12-04 14:29:35 +0100

    lol

--- boids.py
+++ boids.py
@@ -1,47 +1,49 @@
-import maya.cmds as mc
+import maya.cmds as cmds
 import random
 
 distance = 10
-timedelta = 2000
+timedelta = 3000
 startval = 0
-objects = 30
-timestep = 40
+objects = 70
+timestep = 60
 
 cubes = []
 cubeposX = []
 cubeposY = []
 cubeposZ = []
 
-def createObjects:
-    for x in range(objects):
-        tmp = mc.polyCube(constructionHistory=True, width=1, height=1, depth=1)
-        cubes.append(tmp)
-        zCord = random.random()*10
-        xCord = random.random()*10
-        cubeposX.append(xCord)
-        cubeposZ.append(zCord)
-        mc.setKeyframe(tmp, time=1, v=zCord, at='translateZ')
-        mc.setKeyframe(tmp, time=1, v=xCord, at='translateX')
+def boid(): 
+	_position = 0
+	_velocity = 0
+	
 
-    return True
-    
-def simulate:
-    for x in range(timedelta/timestep):
-        x = x * timestep
-        for cube in range(objects):
-            cubeposX[cube] += random.random()*2 - 1
-            cubeposZ[cube] += random.random()*2 - 1
+def createObjects():
+	for x in range(objects):
+		name = 'cube%s' % x
+		tmp = cmds.polyCube(constructionHistory=True, width=1, height=1, depth=1, n=name)
+		cubes.append(tmp)
+		zCord = random.random()*10
+		xCord = random.random()*10
+		cubeposX.append(xCord)
+		cubeposZ.append(zCord)
+		cmds.setKeyframe(tmp, time=1, v=zCord, at='translateZ')
+		cmds.setKeyframe(tmp, time=1, v=xCord, at='translateX')
 
-            mc.setKeyframe(cubes[cube], time=x, v=cubeposZ[cube], at='translateZ')
-            mc.setKeyframe(cubes[cube], time=x, v=cubeposX[cube], at='translateX')
+	return True
+	
+def simulate():
+	for x in range(timedelta/timestep):
+		x = x * timestep
+		for cube in range(objects):
+			cubeposX[cube] += random.random()*2 - 1
+			cubeposZ[cube] += random.random()*2 - 1
 
-    return True
-        
-def main:
-    createObjects()
-    set = cmds.sets()
-    print cmds.select('set')
+			cmds.setKeyframe(cubes[cube], time=x, v=cubeposZ[cube], at='translateZ')
+			cmds.setKeyframe(cubes[cube], time=x, v=cubeposX[cube], at='translateX')
 
-main()
-
-# mc.play()
+	return True
+		
+def main():
+	createObjects()
+	simulate()
+	cmds.play()
