eef37bb1939dd08863747b8eb4bd6d4f9d5e3de4
Markus Waltre <markus.waltre@gmail.com>
2014-12-04 12:59:25 +0100

    lol

--- boids.py
+++ boids.py
@@ -5,28 +5,43 @@ distance = 10
 timedelta = 2000
 startval = 0
 objects = 30
+timestep = 40
 
 cubes = []
 cubeposX = []
 cubeposY = []
 cubeposZ = []
-for x in range(objects):
-    tmp = mc.polyCube(constructionHistory=True, width=1, height=1, depth=1)
-    cubes.append(tmp)
-    zCord = random.random()*10
-    xCord = random.random()*10
-    cubeposX.append(xCord)
-    cubeposZ.append(zCord)
-    mc.setKeyframe(tmp, time=1, v=zCord, at='translateZ')
-    mc.setKeyframe(tmp, time=1, v=xCord, at='translateX')
 
-for x in range(timedelta):
+def createObjects:
+    for x in range(objects):
+        tmp = mc.polyCube(constructionHistory=True, width=1, height=1, depth=1)
+        cubes.append(tmp)
+        zCord = random.random()*10
+        xCord = random.random()*10
+        cubeposX.append(xCord)
+        cubeposZ.append(zCord)
+        mc.setKeyframe(tmp, time=1, v=zCord, at='translateZ')
+        mc.setKeyframe(tmp, time=1, v=xCord, at='translateX')
+
+    return True
     
-    for cube in range(objects):
-        cubeposX[cube] += random.random()*2 - 1
-        cubeposZ[cube] += random.random()*2 - 1
+def simulate:
+    for x in range(timedelta/timestep):
+        x = x * timestep
+        for cube in range(objects):
+            cubeposX[cube] += random.random()*2 - 1
+            cubeposZ[cube] += random.random()*2 - 1
 
-        mc.setKeyframe(cubes[cube], time=x, v=cubeposZ[cube], at='translateZ')
-        mc.setKeyframe(cubes[cube], time=x, v=cubeposX[cube], at='translateX')
+            mc.setKeyframe(cubes[cube], time=x, v=cubeposZ[cube], at='translateZ')
+            mc.setKeyframe(cubes[cube], time=x, v=cubeposX[cube], at='translateX')
 
-mc.play()
+    return True
+        
+def main:
+    createObjects()
+    set = cmds.sets()
+    print cmds.select('set')
+
+main()
+
+# mc.play()
